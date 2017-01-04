36b0c26610c6f0504c878fe025874ef9a72f257f
Sherman <shc410@gmail.com>
2017-01-03 16:25:45 -0800

    successfully retreive awe data

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,9 +4,37 @@ import ReactDOM from 'react-dom';
 import Hello from './Hello';
 
 class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			products: []
+		}
+	}
+
+	componentDidMount() {
+		// on mount make GET request for data
+		fetch('https://sneakpeeq-sites.s3.amazonaws.com/interviews/ce/feeds/store.js')
+		.then(res => {
+			// resolve promise
+			res.json()
+			.then(data => {
+				// update state to grab products info
+				const products = data.products;
+				this.setState({
+					products: products
+				})
+			});
+		})
+		.catch(err => {
+			console.error(err);
+		})
+	}
+
 	render() {
 		return (
-			<Hello />
+			<div>
+				<Hello />
+			</div>
 		)
 	}
 }
