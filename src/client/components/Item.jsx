b1d6983753cc601e455a9f18a71dfb5f11c90d30
Sherman <shc410@gmail.com>
2017-01-03 19:32:54 -0800

    minor linting

--- src/client/components/Item.jsx
+++ src/client/components/Item.jsx
@@ -1,21 +1,5 @@
 import React from 'react';
 
-// const Item = (props) => {
-// 	const url = `https:${props.image}`
-
-// 	return (
-// 		<div className="items_container">
-// 			<label><input type="checkbox" id="wsbox" value="test" /> Wholesale?</label>
-// 			<img className="item_img" src={url} alt={props.alt} />
-// 			<h3 className="item_price">${(props.price / 100).toFixed(2)}</h3>
-// 			<h3 className="item_name">{props.name}</h3>
-// 			<button onClick={props.addToCart} className="cart_btn">Add to Cart</button>
-// 		</div>
-// 	)
-// }
-
-// export default Item
-
 export default class Item extends React.Component {
 	constructor() {
 		super();
